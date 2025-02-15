545e189b2629cd99974cfc181180e259ff8bdfba
hamza aboshhiwa <42789638+Hamza-Shewa@users.noreply.github.com>
2025-02-15 02:32:59 +0200

    Update OneSignalPlugin.java

--- android/src/main/java/com/onesignal/flutter/OneSignalPlugin.java
+++ android/src/main/java/com/onesignal/flutter/OneSignalPlugin.java
@@ -7,14 +7,11 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 
 import com.onesignal.OneSignal;
-import com.onesignal.Continue;
 import com.onesignal.common.OneSignalWrapper;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import androidx.annotation.NonNull;
@@ -22,25 +19,24 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.view.FlutterNativeView;
 
-/** OnesignalPlugin */
-public class OneSignalPlugin extends FlutterRegistrarResponder implements FlutterPlugin, MethodCallHandler, ActivityAware {
+/** OneSignalPlugin */
+public class OneSignalPlugin extends FlutterRegistrarResponder implements FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
+
+  private Context context;
+  private MethodChannel channel;
+  private BinaryMessenger messenger;
 
   public OneSignalPlugin() {
   }
 
-  private void init(Context context, BinaryMessenger messenger)
-  { 
+  private void init(Context context, BinaryMessenger messenger) {
     this.context = context;
     this.messenger = messenger;
-    OneSignalWrapper.setSdkType("flutter");  
-    // For 5.0.0, hard code to reflect SDK version
+    OneSignalWrapper.setSdkType("flutter");
     OneSignalWrapper.setSdkVersion("050209");
-    
+
     channel = new MethodChannel(messenger, "OneSignal");
     channel.setMethodCallHandler(this);
 
@@ -54,11 +50,8 @@ public class OneSignalPlugin extends FlutterRegistrarResponder implements Flutte
   }
 
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding flutterPluginBinding) {
-    init(
-        flutterPluginBinding.getApplicationContext(),
-        flutterPluginBinding.getBinaryMessenger()
-    );
+  public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
+    init(binding.getApplicationContext(), binding.getBinaryMessenger());
   }
 
   @Override
@@ -67,6 +60,7 @@ public class OneSignalPlugin extends FlutterRegistrarResponder implements Flutte
   }
 
   private void onDetachedFromEngine() {
+    // Clean up resources if needed
   }
 
   @Override
@@ -75,41 +69,39 @@ public class OneSignalPlugin extends FlutterRegistrarResponder implements Flutte
   }
 
   @Override
-  public void onDetachedFromActivity() {
-  }
+  public void onDetachedFromActivityForConfigChanges() {}
 
   @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-  }
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {}
 
   @Override
-  public void onDetachedFromActivityForConfigChanges() {
-  }
-
-  // This static method is only to remain compatible with apps that don’t use the v2 Android embedding.
-  @Deprecated()
-  @SuppressLint("Registrar")
-  public static void registerWith(Registrar registrar) {
-    final OneSignalPlugin plugin = new OneSignalPlugin();
-    plugin.init(registrar.activeContext(), registrar.messenger());
-
+  public void onDetachedFromActivity() {}
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.contentEquals("OneSignal#initialize"))
-      this.initWithContext(call, result);
-    else if (call.method.contentEquals("OneSignal#consentRequired"))
-      this.setConsentRequired(call, result);
-    else if (call.method.contentEquals("OneSignal#consentGiven"))
-      this.setConsentGiven(call, result);
-    else if (call.method.contentEquals("OneSignal#login"))
-      this.login(call, result);
-      else if (call.method.contentEquals("OneSignal#loginWithJWT"))
-      this.loginWithJWT(call, result);
-    else if (call.method.contentEquals("OneSignal#logout"))
-      this.logout(call, result);
-    else
-      replyNotImplemented(result);
+    switch (call.method) {
+      case "OneSignal#initialize":
+        initWithContext(call, result);
+        break;
+      case "OneSignal#consentRequired":
+        setConsentRequired(call, result);
+        break;
+      case "OneSignal#consentGiven":
+        setConsentGiven(call, result);
+        break;
+      case "OneSignal#login":
+        login(call, result);
+        break;
+      case "OneSignal#loginWithJWT":
+        loginWithJWT(call, result);
+        break;
+      case "OneSignal#logout":
+        logout(call, result);
+        break;
+      default:
+        replyNotImplemented(result);
+        break;
+    }
   }
 
   private void initWithContext(MethodCall call, Result reply) {
@@ -136,10 +128,13 @@ public class OneSignalPlugin extends FlutterRegistrarResponder implements Flutte
   }
 
   private void loginWithJWT(MethodCall call, Result result) {
-    OneSignal.login((String) call.argument("externalId"), (String) call.argument("jwt"));
+    OneSignal.login(
+      (String) call.argument("externalId"),
+      (String) call.argument("jwt")
+    );
     replySuccess(result, null);
   }
-  
+
   private void logout(MethodCall call, Result result) {
     OneSignal.logout();
     replySuccess(result, null);
